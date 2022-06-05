4f709c3cf12001ce32a9fecd8a98e0814bf774c7
Hatim Lagzar <lhatimdev@gmail.com>
2022-06-05 18:21:34 +0100

    user_id should be string not int

--- database/migrations/2022_06_05_164001_create_relationships_table.php
+++ database/migrations/2022_06_05_164001_create_relationships_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,8 +14,8 @@ return new class extends Migration
     {
         Schema::create('relationships', function (Blueprint $table) {
             $table->uuid('id');
-            $table->unsignedBigInteger('user_id1');
-            $table->unsignedBigInteger('user_id2');
+            $table->string('user_id1');
+            $table->string('user_id2');
             $table->timestamps();
         });
     }
